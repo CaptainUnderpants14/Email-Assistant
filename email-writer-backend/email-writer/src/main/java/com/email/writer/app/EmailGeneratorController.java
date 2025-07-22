03f389f6a6deea382cdfb051d006c1f03251d32e
Paras <parassharma8890@gmail.com>
2025-07-22 22:22:36 +0530

    Backend Code

---
+++ email-writer-backend/email-writer/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -0,0 +1,24 @@
+package com.email.writer.app;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+@Data
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailrequest){
+        String response = emailGeneratorService.generateEmailReply(emailrequest);
+        return ResponseEntity.ok(response);
+    }
+}
